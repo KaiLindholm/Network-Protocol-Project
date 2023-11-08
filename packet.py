d465c387fec00e42e6533eeab62700829b200740
KaiLindholm <kailindholm20022@gmail.com>
2023-11-07 18:26:53 -0600

    Added payload object that will be sent over TCP

---
+++ packet.py
@@ -0,0 +1,10 @@
+
+class Payload:
+    def __init__(self, Cone_ID, IP_Address, Packet_ID, Data_Length, Data_Payload):
+        self.Cone_ID = Cone_ID
+        self.IP_Address = IP_Address
+        self.Packet_ID = Packet_ID
+        self.Data_Length = Data_Length
+        self.Data_Payload = Data_Payload
+        
+    
