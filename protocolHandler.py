eb22412d036e554deee401a6def458c688d04bca
KaiLindholm <kailindholm20022@gmail.com>
2023-11-17 15:36:10 -0600

    comments

--- protocolHandler.py
+++ protocolHandler.py
@@ -12,12 +12,12 @@ class protocolHandler:   # this handles sending and recieving data packets by th
 
     def send_payload(self, socket, payload: DataPacket) -> bool: 
         serialized_payload = payload.to_json()
-        try:
+        try:            # try to send the payload
             socket.sendall(serialized_payload.encode('utf-8'))
-        except Exception as e:
+        except Exception:           # if not successful, return False 
             return False
         
-    def get_host_ip(self):
+    def get_host_ip(self):          # get the host IP address
         try:
             # get the host IP address by connecting to Google's DNS server
             with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
